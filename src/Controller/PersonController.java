43dc09817cfb5aa0ca560f8976b573ff780d0164
Adrian <adri.peters@outlook.com>
2020-04-23 15:03:39 +0200

    PersonController Vorbereitung

---
+++ src/Controller/PersonController.java
@@ -0,0 +1,29 @@
+package Controller;
+
+import Model.Employee;
+
+public class PersonController {
+
+    private PersonController(){} // Klasse soll nur statisch verwendet werden
+
+    public static void add_employee(String firstname, String lastname, String username, String password){
+        // Check Sql Username exists
+        // true --> notify user and enter new username or abort
+        // false -->
+        // Create Sql New Employee
+        // notify user it was successfully
+
+        // (id will be generate by sql server)
+    }
+
+    public static void add_customer(String firstname, String lastname, String username, String password){
+        // Check Sql Username exists
+        // true --> notify user and enter new username or abort
+        // false -->
+        // Create Sql New Employee
+        // notify user it was successfully
+
+        // (id will be generate by sql server)
+    }
+
+}
