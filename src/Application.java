6dcbd04c5530a4484fc2f2746f3b64d4940a4157
Adrian <adri.peters@outlook.com>
2020-04-29 16:15:11 +0200

    UML

--- src/Application.java
+++ src/Application.java
@@ -1,20 +1,8 @@
-import Controller.ShoppingCartController;
-import Model.*;
-import Utilities.PersonType;
 import View.ConsoleTest;
 
 public class Application {
     public static void main(String[] args){
         ConsoleTest console = new ConsoleTest();
         console.start();
-
-        Article article = new Article("TestA", 12345, 50, 30);
-
-        ShoppingCart shoppingCart = new ShoppingCart();
-        ShoppingCartController shoppingCartController = new ShoppingCartController();
-        shoppingCartController.addArticle(shoppingCart, article, 10);
-        shoppingCartController.addArticle(shoppingCart, article, 50);
-
-
     }
 }
