812b77dd2fe2803094a22ec0163f17a28a7e69d2
Adrian Peters <adri.peters@outlook.com>
2020-06-11 13:17:59 +0200

    Verpackungseinheit

--- src/Utilities/ArticleExtension.java
+++ src/Utilities/ArticleExtension.java
@@ -1,4 +1,21 @@
 package Utilities;
 
+import Model.Article;
+import Model.BulkArticle;
+
 public class ArticleExtension {
+    private ArticleExtension() {
+    } // static
+
+    public static int getPackagingUnit(Article article) {
+        int unit = 1;
+
+            String className = article.getClass().getSimpleName();
+
+            if (className.equals(BulkArticle.class.getSimpleName())) {
+                unit = ((BulkArticle)article).getPackagingUnit();
+            }
+
+        return unit;
+    }
 }
