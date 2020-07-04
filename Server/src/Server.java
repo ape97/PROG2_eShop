9f8d8428c9de0b6c01b9b797651c1468dcf3ce83
Adrian Peters <adri.peters@outlook.com>
2020-07-04 14:26:44 +0200

    fix

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -1,16 +1,21 @@
 import Communication.ServerController;
-import Controller.DataController;
-
-
 import java.io.IOException;
 import java.util.Scanner;
 
+/**
+ * Der Einstiegpunkt der Server-Anwendung.
+ */
 public class Server {
 
     public static void main(String[] args) throws IOException {
+        // Startet den ServerController, dieser startet den ClientRegisterProcessor
         ServerController serverController = new ServerController();
         serverController.start();
 
+        // Beendet den Server erst, wenn in die Console "stop" eingegeben wurde.
+        // WICHTIG: Ohne die Eingabe von "stop", werden die Daten NICHT gespeichert!
+        //          Der Server muss stets korrekt beendet werden!
+        System.out.println("Um die Daten zu SPEICHERN und den Server zu BEENDEN, bitte 'stop' eingeben!!!");
         while (!readConsoleInput().equals("stop")) {
             // Wartet bis der Stopp Befehl eingegebn wurde
         }
